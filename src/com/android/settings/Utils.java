549b512a040f279019bbf4feecaafaf577858b05
D4rKn3sSyS <jesuelfather@hotmail.com>
2012-06-28 16:44:22 -0500

    Fixed isScreenLarge method for new sources

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -490,8 +490,7 @@ public class Utils {
     }
 
     public static boolean isScreenLarge() {
-        int mDefaultDensity = Integer.parseInt(ExtendedPropertiesUtils.getProperty("%rom_phone_base", "", true));
-        return SystemProperties.getInt("ro.sf.lcd_density", mDefaultDensity) <= Integer.parseInt(ExtendedPropertiesUtils.getProperty("%rom_tablet_base", String.valueOf(mDefaultDensity), true));
+        return Integer.parseInt(ExtendedPropertiesUtils.getProperty("com.android.systemui.mode", "2", true)) == 2;
     }
 
 }
